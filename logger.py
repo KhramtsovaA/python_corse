38565c6152569e200b9d1a0528efaaaaee260976
KhramtsovaA <Khramtsova.anna2016@gmail.com>
2024-07-02 12:52:01 +0300

    Data_create

---
+++ logger.py
@@ -0,0 +1,19 @@
+from data_create import name_data,surname_data ,phone_data,address_data
+
+def input_data():
+    name = name_data()
+    surname = surname_data()
+    phone = phone_data()
+    address = address_data()
+    var = int(input(f"В каком формате записать данные\n\n"
+    f"1 Вариант: \n"
+    f"{name}\n {surname}\n{phone}\n{address}\n\n"
+    f"2 Вариант: \n"
+    f"{name};{surname};{phone};{address}\n"
+    f"Выберите вариант: "))
+
+
+def print_data():
+    pass
+
+input_data()
