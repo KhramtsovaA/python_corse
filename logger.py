d45aa88d99d1881c30d479841459c16aacd93449
KhramtsovaA <Khramtsova.anna2016@gmail.com>
2024-07-04 10:07:31 +0300

    All changes

--- logger.py
+++ logger.py
@@ -11,9 +11,34 @@ def input_data():
     f"2 Вариант: \n"
     f"{name};{surname};{phone};{address}\n"
     f"Выберите вариант: "))
-
+    
+    while var != 1 and var != 2:
+        print("Неправильный ввод")
+        var = int(input('Введите число '))
+    
+    if var == 1:
+        with open('data_first_variant.csv', 'a', encoding='utf-8') as f:
+            f.write(f"{name}\n{surname}\n{phone}\n{address}\n\n")
+    elif var == 2:
+        with open('data_second_variant.csv', 'a', encoding='utf-8') as f:
+            f.write(f"\n{name};{surname};{phone};{address}\n\n")
 
 def print_data():
-    pass
+    print('Вывожу данные из 1 файла: \n')
+    with open('data_first_variant.csv', 'r', encoding='utf-8') as f:
+        data_first = f.readlines()
+        data_first_list = []
+        j = 0
+        for i in range (len(data_first)):
+            if data_first[i] == '\n' or i == len(data_first) - 1:
+                data_first_list.append(''.join(data_first[j:i+1]))
+                j = i
+        print (''.join(data_first_list)) 
+        
+    print('Вывожу данные из 2 файла: \n')
+    with open('data_second_variant.csv', 'r', encoding='utf-8') as f:
+        data_second  = f.readlines()
+        print(*data_second)
 
-input_data()
+
+print_data()
--- ui.py
+++ ui.py
@@ -1,11 +1,28 @@
-def interface():
-    print("Добрый день! Вы попали на специальный бот от Geekbrains! \n 1- запись данных \n 2 - вывод данных ")
-    command = int(input('Введите число '))
+# ui.py
+from logger import input_data, print_data
+from rename_delete import update_data, delete_data
 
-    while command != 1 and command != 2:
-        print("Неправильный ввод")
-        command = int(input('Введите число '))
-    if command == 1:
-        input data()
-    elif command == 2:
-        print data()
+def interface():
+    while True:
+        print("\nДобро пожаловать в телефонный справочник!")
+        print("1 - Записать новые данные")
+        print("2 - Вывести существующие данные")
+        print("3 - Изменить данные")
+        print("4 - Удалить данные")
+        print("5 - Выйти из программы")
+        
+        command = input("Введите номер команды: ")
+        
+        if command == "1":
+            input_data()
+        elif command == "2":
+            print_data()
+        elif command == "3":
+            update_data()
+        elif command == "4":
+            delete_data()
+        elif command == "5":
+            print("Выход из программы...")
+            break
+        else:
+            print("Неверная команда. Пожалуйста, попробуйте снова.")
